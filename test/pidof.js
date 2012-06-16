8b1dd4cde4d5be9478e6c56741507ef8d85e5891
Jakob Borg <jakob@nym.se>
2012-06-16 13:04:30 +0200

    Get pid of running process.

---
+++ test/pidof.js
@@ -0,0 +1,24 @@
+var should = require('should');
+
+var output = '';
+output += '  PID COMMAND\n';
+output += ' 3574 /usr/lib/utmpd\n';
+output += ' 7213 /usr/lib/saf/ttymon\n';
+output += ' 3039 zsched\n';
+output += ' 3138 /sbin/init\n';
+output += ' 3211 /lib/inet/ipmgmtd\n';
+output += ' 3165 /lib/svc/bin/svc.startd\n';
+
+var pidof = require('../lib/pidof');
+
+describe('pidof', function () {
+    it('should return null for no match', function () {
+        var pid = pidof.parse(output, 'foo');
+        should.not.exist(pid);
+    });
+    it('should recognize output', function () {
+        var pid = pidof.parse(output, 'zsched');
+        should.exist(pid);
+        pid.should.equal(3039);
+    });
+});
