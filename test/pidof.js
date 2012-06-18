d16f4e7edf4d43f1a4d80e2235d0d042ffbaf3b5
Jakob Borg <jakob@nym.se>
2012-06-18 10:02:02 +0200

    Also add negative test to be really sure.

--- test/pidof.js
+++ test/pidof.js
@@ -11,7 +11,7 @@ output += ' 3165 /lib/svc/bin/svc.startd\n';
 
 var pidof = require('../lib/pidof');
 
-describe('pidof', function () {
+describe('pidof parser', function () {
     it('should return null for no match', function () {
         var pid = pidof.parse(output, 'foo');
         should.not.exist(pid);
@@ -21,10 +21,20 @@ describe('pidof', function () {
         should.exist(pid);
         pid.should.equal(3039);
     });
+});
+
+describe('pidoff callback', function () {
     it('should find "node" running during tests', function (done) {
         pidof('node', function (err, pid) {
             should.not.exist(err);
             should.exist(pid);
+            done();
+        });
+    });
+    it('should not find very unlikely process name running during tests', function (done) {
+        pidof('0FB001AB-CE37-4E43-A7DB-1FD8BD5D812C', function (err, pid) {
+            should.not.exist(err);
+            should.not.exist(pid);
             done();
         });
     });
